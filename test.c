4a735493cf7ee9ec9c44e228d0549e01ffcf479d
Zakaria El Yamani <zel-yama@e2r9p15.1337.ma>
2025-01-21 10:23:08 +0100

    q

--- test.c
+++ test.c
@@ -5,6 +5,7 @@
 int main()
 {
     int i = open("file.txt", O_WRONLY | O_CREAT, 0777);
+    // int i = open("file.txt", O_WRONLY | O_CREAT, 0777);
    
    dup2(i, 1);
     printf("output to this file don't freez your self\n");
